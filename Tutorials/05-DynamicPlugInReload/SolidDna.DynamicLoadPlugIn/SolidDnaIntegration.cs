94e307509dca834fd34a6b63905ab415abdb37da
Luke Malpass <contact@angelsix.com>
2020-05-13 15:10:34 +0100

    Documentation for detached app domain

--- Tutorials/05-DynamicPlugInReload/SolidDna.DynamicLoadPlugIn/SolidDnaIntegration.cs
+++ Tutorials/05-DynamicPlugInReload/SolidDna.DynamicLoadPlugIn/SolidDnaIntegration.cs
@@ -1,5 +1,4 @@
 ﻿using AngelSix.SolidDna;
-using Dna;
 
 namespace SolidDna.DynamicLoadPlugIn
 {
@@ -22,8 +21,9 @@ namespace SolidDna.DynamicLoadPlugIn
         /// <returns></returns>
         public override void PreLoadPlugIns()
         {
-            PlugInIntegration.AddPlugIn(@"D:\git\solidworks-api\Tutorials\05-DynamicPlugInReload\SolidDna.DynamicLoadPlugIn\bin\Debug\SolidDna.DynamicLoadPlugIn.Main.dll");
+
         }
+
 
         public override void PreConnectToSolidWorks()
         {
@@ -31,12 +31,27 @@ namespace SolidDna.DynamicLoadPlugIn
             //       Be aware doing so sometimes causes API's to fail
             //       when they try to load dll's
             //
+            //      To use, keep your SolidPlugIn implementation in separate
+            //      projects, and just this add-in on its own
+            //
+            //      Right-click solution and select Configuration Manager
+            //      Uncheck this project and AngelSix.SolidDNA "Build" box
+            //      so building the project does not build them
+            //      or alternatively when building right click the specific
+            //      project containing the SolidPlugIn and select Build
+            //
+            //      Run the code as normal having this project as startup
+            //      and running SLDWORKS.exe. When you want to make a change
+            //      go in SolidWorks to Add-ins... and unload this one
+            //
+            //      Then in Visual Studio select Debug Detach All
+            //      Now SolidWorks is still open, you can edit the SolidPlugIn
+            //      project code, and re-build the projects, then select
+            //      Debug > Attach to Process... and find SLDWORKS.exe
+            //      and now Add-ins.. and load the add-in. All changes will
+            //      be loaded in.
+            //  
             AppDomainBoundary.UseDetachedAppDomain = true;
-        }
-
-        public override void ConfigureServices(FrameworkConstruction construction)
-        {
-            base.ConfigureServices(construction);
         }
     }
 }
