6c510566ea87ce0f73dba80726908617415543c0
CADBooster-Peter <peter@cadbooster.com>
2021-09-18 12:11:20 +0200

    Added column to show whether the DLL is in the Debug or Release folder

---
+++ Tools/Addin Installer/PathToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AngelSix.SolidWorksApi.AddinInstaller
+{
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class PathToVisibilityConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+            var path = (string)value;
+            var parameterString = (string)parameter;
+
+            return path.ToLower().Contains(parameterString.ToLower()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
